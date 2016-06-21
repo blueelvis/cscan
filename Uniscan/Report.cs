4a19897cf71a313e174ce7af7127433be79ecb73
Ian Carroll <ian@ian.sh>
2016-06-21 00:17:07 -0400

    Report and ToString

---
+++ Uniscan/Report.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uniscan
+{
+    class Report
+    {
+        protected List<String> lines = new List<String>();
+
+        public void Add(string line)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            string output = "";
+
+            foreach(string line in lines)
+            {
+                output = output + line + Environment.NewLine;
+            }
+
+            return output;
+        }
+    }
+}
