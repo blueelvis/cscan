40807ff0138a26e44f333f9cb372440f65c6623b
Ian Carroll <ian@ian.sh>
2016-06-24 18:56:28 -0400

    Adds a decrypter application.

---
+++ CScanDecrypter/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CScanDecrypter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generateKey_Click(object sender, EventArgs e)
+        {
+            ECDiffieHellmanCng self = new ECDiffieHellmanCng();
+            self.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash;
+            self.HashAlgorithm = CngAlgorithm.Sha256;
+
+            string key = Convert.ToBase64String(self.PublicKey.ToByteArray());
+
+            Clipboard.SetText(key);
+
+            MessageBox.Show("Your key has been copied to the clipboard.", "Successfully Generated Key", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
