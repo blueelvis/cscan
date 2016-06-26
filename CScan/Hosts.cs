008ee48aa7c5e30761f6abbe835e82a7ad94db70
Ian Carroll <ian@ian.sh>
2016-06-26 01:50:00 -0400

    Hosts file

---
+++ CScan/Hosts.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CScan.Components
+{
+    class Hosts : Component
+    {
+        public bool Run(ref CScan.Report report, List<List<KeyValuePair<string, string>>> list)
+        {
+            string[] hosts = File.ReadAllLines(@"C:\Windows\System32\drivers\etc\hosts");
+
+            foreach (string line in hosts)
+            {
+                if (line != "" && line.Substring(0, 1) != "#")
+                {
+                    list.Add(new List<KeyValuePair<string, string>>() {
+                        new KeyValuePair<string, string>("token", "Hst"),
+                        new KeyValuePair<string, string>("contents", line),
+                    });
+                }
+            }
+
+            if (list.Count > 0)
+            {
+                report.Add(list);
+            }
+
+            return true;
+        }
+    }
+}
--- CScan/Scanner.cs
+++ CScan/Scanner.cs
@@ -15,6 +15,7 @@ namespace CScan
             "IEProxy",
             "Processes",
             "RegistryRun",
+            "Hosts",
             "Services",
             "Drivers",
             "Disks",
