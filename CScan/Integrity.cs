96b0ab40a263d795ec5b6eef9f5583df42692cc9
Ian Carroll <ian@ian.sh>
2016-06-26 02:19:45 -0400

    Integrity

---
+++ CScan/Integrity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CScan.Components
+{
+    class Integrity : Component
+    {
+        public bool Run(ref CScan.Report report, List<List<KeyValuePair<string, string>>> list)
+        {
+            return true;
+        }
+    }
+}
