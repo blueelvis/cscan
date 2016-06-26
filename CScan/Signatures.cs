84f7970e506b8c8b111883ef9385e546c447f138
Ian Carroll <ian@ian.sh>
2016-06-25 20:10:47 -0400

    Sig

--- CScan/Signatures.cs
+++ CScan/Signatures.cs
@@ -13,16 +13,21 @@ namespace CScan.Components
         public string[] files =
         {
             @"C:\Windows\explorer.exe",
+            @"C:\Windows\System32\userinit.exe",
+            @"C:\Windows\System32\svchost.exe",
+            @"C:\Windows\System32\wininit.exe",
         };
 
         public bool Run(ref CScan.Report report, List<List<KeyValuePair<string, string>>> list)
         {
             foreach (string file in files)
             {
+                bool exists = File.Exists(file);
+
                 list.Add(new List<KeyValuePair<string, string>>() {
                     new KeyValuePair<string, string>("token", "Sig"),
                     new KeyValuePair<string, string>("file", file),
-                    new KeyValuePair<string, string>("signed", !Authenticode.IsSigned(file, true) ? "[b]is not signed[/b]" : "is signed"),
+                    new KeyValuePair<string, string>("signed", exists ? !Authenticode.IsSigned(file, true) ? "[b]is not signed[/b]" : "is signed" : "[b]does not exist[/b]"),
                 });
             }
 
