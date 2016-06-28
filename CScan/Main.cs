04c1f7d3d77ade8ce3071cdd1a608715add79645
Ian Carroll <ian@ian.sh>
2016-06-28 15:21:07 -0400

    License

--- CScan/Main.cs
+++ CScan/Main.cs
@@ -23,6 +23,8 @@ namespace CScan
 
             Process.EnterDebugMode();
             Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.RealTime;
+
+            MessageBox.Show(name + " is distributed by Certly Inc under the Apache 2.0 license (the \"License\")." + Environment.NewLine + Environment.NewLine + "Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.");
         }
 
         private void Scan_Click(object sender, EventArgs e)
