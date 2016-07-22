ae4622eeaaaedfb886f84adf5da515340ad961a0
Ian Carroll <ian@ian.sh>
2016-07-21 21:05:07 -0400

    Remove the file iterator from FileInspector.

--- CScan/FileInspector.cs
+++ CScan/FileInspector.cs
@@ -33,42 +33,5 @@ namespace CScan
 
             return companyName;
         }
-
-        public static IEnumerable<string> GetFiles(string path)
-        {
-            var queue = new Queue<string>();
-            queue.Enqueue(path);
-            while (queue.Count > 0)
-            {
-                path = queue.Dequeue();
-                try
-                {
-                    foreach (var subDir in Directory.GetDirectories(path))
-                    {
-                        queue.Enqueue(subDir);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.Error.WriteLine(ex);
-                }
-                string[] files = null;
-                try
-                {
-                    files = Directory.GetFiles(path);
-                }
-                catch (Exception ex)
-                {
-                    Console.Error.WriteLine(ex);
-                }
-                if (files != null)
-                {
-                    for (var i = 0; i < files.Length; i++)
-                    {
-                        yield return files[i];
-                    }
-                }
-            }
-        }
     }
 }
