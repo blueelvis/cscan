593cd95fbf5daf3a29a8d8def05b38310fea2a17
Ian Carroll <ian@certly.io>
2016-08-21 02:04:30 -0400

    Avoid naming conflicts.

--- CScan/RegistryWrapper/RegistryResult.cs
+++ CScan/RegistryWrapper/RegistryResult.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CScan.Registry
+namespace CScan.RegistryWrapper
 {
     enum RegistryKeyArchitecture
     {
--- CScan/RegistryWrapper/RegistryWrapper.cs
+++ CScan/RegistryWrapper/RegistryWrapper.cs
@@ -1,13 +1,8 @@
 ﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CScan.Registry
+namespace CScan.RegistryWrapper
 {
-    class Registry
+    class RegistryWrapper
     {
         public static RegistryResult[] QueryKey(RegistryKey baseKey)
         {
