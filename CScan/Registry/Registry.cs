5758014c315003c55e86383d2c0ad0ded6c3e603
Ian Carroll <ian@certly.io>
2016-08-21 01:58:22 -0400

    Start on a registry wrapper.

---
+++ CScan/Registry/Registry.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CScan.Registry
+{
+    class Registry
+    {
+        public static RegistryResult[] QueryKey(RegistryKey baseKey)
+        {
+            return new RegistryResult[] { };
+        }
+    }
+}
---
+++ CScan/Registry/RegistryResult.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CScan.Registry
+{
+    enum RegistryKeyArchitecture
+    {
+        amd64,
+        i386,
+    };
+
+    struct RegistryResult
+    {
+        public RegistryKey key;
+        public RegistryKeyArchitecture architecture;
+
+        public RegistryResult(RegistryKey key, RegistryKeyArchitecture architecture)
+        {
+            this.key = key;
+            this.architecture = architecture;
+        }
+    }
+}
