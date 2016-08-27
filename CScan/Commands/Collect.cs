bc69d4df91343739d95bdada1f12b6edbcc82d52
Ian Carroll <ian@ian.sh>
2016-08-27 01:25:26 -0400

    Adds the Collect command (closes #23).

--- CScan/Commands/Collect.cs
+++ CScan/Commands/Collect.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 
 namespace CScan.Commands
 {
@@ -6,16 +9,36 @@ namespace CScan.Commands
     {
         public List<Dictionary<string, string>> Run(List<string> arguments, List<Dictionary<string, string>> list)
         {
-            foreach (var line in arguments)
+            var directory = GetTemporaryDirectory();
+            var outputPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Desktop\Collected Files.zip";
+
+            foreach (var file in arguments)
             {
-                list.Add(new Dictionary<string, string>
-                {
-                    {"token", "Collect"},
-                    {"line", line}
-                });
+                var fileName = FileInspector.GetHash(file) + " - " + Path.GetFileName(file);
+
+                File.Copy(file, directory + "\\" + fileName, true);
             }
 
+            if (File.Exists(outputPath))
+                File.Delete(outputPath);
+
+            ZipFile.CreateFromDirectory(directory, outputPath);
+            
+            list.Add(new Dictionary<string, string>()
+            {
+                {"token", "Collect"},
+                {"path", "Created successfully at " + outputPath}
+            });
+
             return list;
+        }
+
+        private string GetTemporaryDirectory()
+        {
+            string tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(tempDirectory);
+
+            return tempDirectory;
         }
     }
 }
