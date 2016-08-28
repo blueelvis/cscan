22b885317bce0287d3818e02f91bea63800dd1d6
Ian Carroll <ian@ian.sh>
2016-08-28 00:57:48 -0400

    Fix files and add more drivers.

--- CScan/Whitelist/DriverWhitelist.cs
+++ CScan/Whitelist/DriverWhitelist.cs
@@ -6,11 +6,28 @@ using System.Threading.Tasks;
 
 namespace CScan.Whitelist
 {
-    class ServiceWhitelist
+    class DriverWhitelist
     {
         private static Dictionary<string, string> whitelist = new Dictionary<string, string>()
         {
-            {@"C:\WINDOWS\system32\svchost.exe -k LocalServiceNetworkRestricted", "AJRouter"},
+            {@"\??\C:\Program Files\NVIDIA Corporation\NvStreamSrv\NvStreamKms.sys", "NvStreamKms"},
+            {@"\??\C:\WINDOWS\system32\drivers\cbfs6.sys", "cbfs6"},
+            {@"\??\C:\WINDOWS\system32\drivers\hcmon.sys", "VMware hcmon"},
+            {@"\??\C:\Windows\system32\drivers\KeyAgent.sys", "KeyAgent"},
+            {@"\??\C:\Windows\system32\drivers\MacHALDriver.sys", "Mac HAL"},
+            {@"C:\WINDOWS\system32\drivers\1394ohci.sys", "1394 OHCI Compliant Host Controller"},
+            {@"C:\WINDOWS\system32\drivers\3ware.sys", "3ware"},
+            {@"C:\WINDOWS\system32\drivers\ACPI.sys", "Microsoft ACPI Driver"},
+            {@"C:\WINDOWS\system32\drivers\acpials.sys", "ALS Sensor Filter"},
+            {@"C:\WINDOWS\system32\drivers\AcpiDev.sys", "ACPI Devices driver"},
+            {@"C:\WINDOWS\system32\Drivers\acpiex.sys", "Microsoft ACPIEx Driver"},
+            {@"C:\WINDOWS\system32\DRIVERS\ahcache.sys", "Application Compatibility Cache"},
+            {@"C:\WINDOWS\system32\drivers\bridge.sys", "Microsoft MAC Bridge"},
+            {@"C:\WINDOWS\system32\Drivers\cng.sys", "CNG"},
+            {@"C:\WINDOWS\system32\drivers\disk.sys", "Disk Driver"},
+            {@"C:\WINDOWS\system32\DRIVERS\drmkaud.sys", "Microsoft Trusted Audio Drivers"},
+            {@"C:\WINDOWS\system32\DRIVERS\fvevol.sys", "BitLocker Drive Encryption Filter Driver"},
+            {@"C:\WINDOWS\system32\drivers\xboxgip.sys", "Xbox Game Input Protocol Driver"},
         };
 
         public static bool IsWhitelisted(string key, string value, bool keyIsSignedFile = false)
