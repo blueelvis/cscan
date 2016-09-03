a401a3296b12e0a79e498b9ee7f9e4bc77eaf7db
Ian Carroll <ian@ian.sh>
2016-09-03 04:19:50 -0400

    Now the Registry command works.

--- CScan/Commands/Registry.cs
+++ CScan/Commands/Registry.cs
@@ -12,7 +12,7 @@ namespace CScan.Commands
         public List<Dictionary<string, string>> Run(List<string> arguments, List<Dictionary<string, string>> list)
         {
             var tempFile = Path.GetTempFileName();
-            list.Insert(0, "Windows Registry Editor Version 5.00");
+            arguments.Insert(0, "Windows Registry Editor Version 5.00");
 
             File.WriteAllLines(tempFile, arguments.ToArray());
 
